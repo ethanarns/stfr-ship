aaa230a445cf6e641adecd798197e815029d1fe0
Ethan Arns <ethanarns95@gmail.com>
2021-10-06 12:25:46 -0700

    Renamings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { StarshipData } from './starship/starship.component';
+import { StarshipData } from './starship/starship-data.component';
 
 @Component({
     selector: 'app-root',
